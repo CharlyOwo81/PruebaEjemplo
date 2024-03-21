6b6cf219ebc5e8e9789825485366bc110996c04e
CharlyOwo81 <150296234+CharlyOwo81@users.noreply.github.com>
2024-03-20 18:58:45 -0700

    Cambio de variable

---
+++ PruebaEjemplo/src/main/java/org/itson/bdavanzadas/pruebaejemplo/PruebaEjemplo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.bdavanzadas.pruebaejemplo;
+
+/**
+ *
+ * @author gamaliel
+ */
+public class PruebaEjemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String nombre;
+    }
+}
